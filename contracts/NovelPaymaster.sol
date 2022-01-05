40f328ade639216901d03b9734ee26f1800b94dc
Ray Deck <ray@raydeck.com>
2022-01-05 15:25:15 -0500

    fix: remove weird checkOwner modifier since we can use onlyOwner from OZ and it works better

--- contracts/NovelPaymaster.sol
+++ contracts/NovelPaymaster.sol
@@ -6,92 +6,105 @@ import "@opengsn/contracts/src/BasePaymaster.sol";
 import "@opengsn/contracts/src/interfaces/IRelayHub.sol";
 import "./NovelCollection.sol";
 
-
 // accept everything.
 // this paymaster accepts any request.
 //
 // NOTE: Do NOT use this contract on a mainnet: it accepts anything, so anyone can "grief" it and drain its account
 
 contract NovelPaymaster is BasePaymaster {
-
     address public owners;
 
-	modifier checkOwner {
-        require(msg.sender == owners || tx.origin == owners, "You're not the owner of the contract");
-        _;
+    constructor(address _relayHubAddress) {
+        setRelayHub(IRelayHub(_relayHubAddress));
     }
 
-	constructor(address _relayHubAddress) {
-		setRelayHub(IRelayHub(_relayHubAddress));
-	}
-	
-	/**
-	  * Withdrawal implemented for safety reasons in case funds are
-	  * accidentally sent to the contract
-	  */
-	function withdraw() external {
-		uint256 balance = address(this).balance;
-		(bool sent, ) = payable(owner()).call{value: balance}("");
-		require(sent);
-	}
+    /**
+     * Withdrawal implemented for safety reasons in case funds are
+     * accidentally sent to the contract
+     */
+    function withdraw() external {
+        uint256 balance = address(this).balance;
+        (bool sent, ) = payable(owner()).call{value: balance}("");
+        require(sent);
+    }
 
-	mapping (address=>bool) public targetWhitelist;
+    mapping(address => bool) public targetWhitelist;
 
-	// this is how we mark a contract as one we will pay for
-	function enableContract(address target) external checkOwner {
-		targetWhitelist[target] = true;
-	}
+    // this is how we mark a contract as one we will pay for
+    function enableContract(address target) external onlyOwner {
+        targetWhitelist[target] = true;
+    }
 
-	function isEnabledContract(address target) external view returns (bool) {
-		if (targetWhitelist[target]) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    function isEnabledContract(address target) external view returns (bool) {
+        if (targetWhitelist[target]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	function preRelayedCall(
-		GsnTypes.RelayRequest calldata relayRequest,
-		bytes calldata signature,
-		bytes calldata approvalData,
-		uint256 maxPossibleGas
-	) external override virtual returns (bytes memory context, bool) {
-		_verifyForwarder(relayRequest);
-		(signature, approvalData, maxPossibleGas);
-		require(targetWhitelist[relayRequest.request.to], "This Target is not added!");
+    function preRelayedCall(
+        GsnTypes.RelayRequest calldata relayRequest,
+        bytes calldata signature,
+        bytes calldata approvalData,
+        uint256 maxPossibleGas
+    ) external virtual override returns (bytes memory context, bool) {
+        _verifyForwarder(relayRequest);
+        (signature, approvalData, maxPossibleGas);
+        require(
+            targetWhitelist[relayRequest.request.to],
+            "This Target is not added!"
+        );
 
         // Only work on mint function
-        NovelCollection novelCollection = NovelCollection(relayRequest.request.to);
-		bytes4 methodSig = GsnUtils.getMethodSig(relayRequest.request.data);
-		require(methodSig == novelCollection.mint.selector, "you can only call mint function" );
-        
-		// TODO: confirm that this actually works (checks that attempted transaction has a quantity that is whitelisted)
-		uint32 quantity = extractMintCountFromCall(relayRequest.request.data);
-		require(novelCollection.canMintAmount(relayRequest.request.from, quantity));
+        NovelCollection novelCollection = NovelCollection(
+            relayRequest.request.to
+        );
+        bytes4 methodSig = GsnUtils.getMethodSig(relayRequest.request.data);
+        require(
+            methodSig == novelCollection.mint.selector,
+            "you can only call mint function"
+        );
 
-		return ("", false);
-	}
+        // TODO: confirm that this actually works (checks that attempted transaction has a quantity that is whitelisted)
+        uint32 quantity = extractMintCountFromCall(relayRequest.request.data);
+        require(
+            novelCollection.canMintAmount(relayRequest.request.from, quantity)
+        );
 
-	function postRelayedCall(
-		bytes calldata context,
-		bool success,
-		uint256 gasUseWithoutPost,
-		GsnTypes.RelayData calldata relayData
-	) external override virtual {
-                (context, success, gasUseWithoutPost, relayData);
-	}
+        return ("", false);
+    }
 
-	function versionPaymaster() external view override virtual returns (string memory){
-	    return "2.2.3+novel-paymaster";
-	}
-	
-	function extractMintCountFromCall(bytes memory b) internal pure returns (uint32){
-		uint32 number;
+    function postRelayedCall(
+        bytes calldata context,
+        bool success,
+        uint256 gasUseWithoutPost,
+        GsnTypes.RelayData calldata relayData
+    ) external virtual override {
+        (context, success, gasUseWithoutPost, relayData);
+    }
 
-		// if this doesn't work then try 6 instead of 4
-		for(uint8 i=4; i<8; i++){
-			number = number + uint32(uint8(b[i])*(2**(8*(8-(i+1)))));
-		}
-		return number;
-	}
+    function versionPaymaster()
+        external
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        return "2.2.3+novel-paymaster";
+    }
+
+    function extractMintCountFromCall(bytes memory b)
+        internal
+        pure
+        returns (uint32)
+    {
+        uint32 number;
+
+        // if this doesn't work then try 6 instead of 4
+        for (uint8 i = 4; i < 8; i++) {
+            number = number + uint32(uint8(b[i]) * (2**(8 * (8 - (i + 1)))));
+        }
+        return number;
+    }
 }
